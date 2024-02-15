f2eac24042773186b2958ec09c119ed4a891f21d
aditya1391998 <160165649+aditya1391998@users.noreply.github.com>
2024-02-15 23:56:31 +0530

    Create port_scanner.py

---
+++ port_scanner.py
@@ -0,0 +1,28 @@
+import socket
+
+def scan_ports(target, start_port, end_port):
+    print(f'Starting port scan on target: {target}')
+    for port in range(start_port, end_port + 1):
+        try:
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            sock.settimeout(1)
+            result = sock.connect_ex((target, port))
+            if result == 0:
+                print(f'Port {port} is open')
+            sock.close()
+        except KeyboardInterrupt:
+            print("\nExiting program.")
+            sys.exit()
+        except socket.gaierror:
+            print("Hostname could not be resolved. Exiting")
+            sys.exit()
+        except socket.error:
+            print("Couldn't connect to server")
+            sys.exit()
+
+if __name__ == "__main__":
+    target = input("Enter the target IP address: ")
+    start_port = int(input("Enter the starting port number: "))
+    end_port = int(input("Enter the ending port number: "))
+
+    scan_ports(target, start_port, end_port)
